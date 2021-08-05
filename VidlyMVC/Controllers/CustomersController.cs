ec52dae32266f4274164fb2e00dad7db04491884
Erikson Ribeiro <eajribeiro@gmail.com>
2021-08-05 11:25:32 -0300

    Boostrap5.0

--- VidlyMVC/Controllers/CustomersController.cs
+++ VidlyMVC/Controllers/CustomersController.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
 using System.Linq;
 using VidlyMVC.Data;
 using VidlyMVC.Models.ViewModels;
+using VidlyMVC.Models;
 
 namespace VidlyMVC.Controllers
 {
